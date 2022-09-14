5e1c035c7e01c80907d9d3c6a79452dd8fb7940b
Olan <kodokpu@gmail.com>
2022-09-14 14:51:38 +0700

    gambar titik

--- main.js
+++ main.js
@@ -1,3 +1,47 @@
-function main(){
+function main() {
+    var kanvas = document.getElementById("kanvas");
+    var gl = kanvas.getContext("webgl");
+
+    // Vertex shader
+    var vertexShaderCode = `
+    precision mediump float;
+    void main () {
+        float x = 0.0;
+        float y = 0.0;
+        gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
+        gl_PointSize = 30.0;
+      }
+      `;
     
+    var vertexShaderObject = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertexShaderObject, vertexShaderCode);
+    gl.compileShader(vertexShaderObject); 
+
+    // Fragment shader
+    var fragmentShaderCode = `
+    precision mediump float;
+    void main () {
+      float r = 0.0;
+      float g = 0.0;
+      float b = 1.0;
+      gl_FragColor = vec4(r, g, b, 1.0);
+    }
+  `;
+    var fragmentShaderObject = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fragmentShaderObject, fragmentShaderCode);
+    gl.compileShader(fragmentShaderObject); 
+
+    var shaderProgram = gl.createProgram(); // wadah dari executable (.exe)
+    gl.attachShader(shaderProgram, vertexShaderObject);
+    gl.attachShader(shaderProgram, fragmentShaderObject);
+    gl.linkProgram(shaderProgram);
+    gl.useProgram(shaderProgram);
+
+    gl.clearColor(1.0, 0.5, 0.0, 1.0); // Merah, Hijau, Biru, Transparansi
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.drawArrays(gl.POINTS, 0, 1);
+
 }
+main();
+//versiku
