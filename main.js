13f742d55cd65237a21fba5de27a1dc9e4d2c3e0
Olan <kodokpu@gmail.com>
2022-09-21 14:25:24 +0700

    event freeze & unfreeze

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ function main() {
     var gl = kanvas.getContext("webgl");
 
     var vertices = [
-        0.5, 0.5, 0.0, 1.0, 1.0,   // A: kanan atas (CYAN)
-        0.0, 0.0, 1.0, 0.0, 1.0,   // B: bawah tengah (MAGENTA)
-        -0.5, 0.5, 1.0, 1.0, 0.0,  // C: kiri atas (KUNING)
-        0.0, 1.0, 1.0, 1.0, 1.0  // D : Tengah atas (PUTIH)
+        0.5, 0.0, 0.0, 1.0, 1.0,   // A: kanan atas (CYAN)
+        0.0, -0.5, 1.0, 0.0, 1.0,   // B: bawah tengah (MAGENTA)
+        -0.5, 0.0, 1.0, 1.0, 0.0,  // C: kiri atas (KUNING)
+        0.0, 0.5, 1.0, 1.0, 1.0  // D : Tengah atas (PUTIH)
     ];
     var buffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
@@ -50,6 +50,7 @@ function main() {
 
     // Variabel lokal
     var theta = 0.0;
+    var freeze = false;
 
     // Variabel pointe rke GLSL
     var uTheta = gl.getUniformLocation(shaderProgram, "uTheta");
@@ -68,12 +69,21 @@ function main() {
         2 * Float32Array.BYTES_PER_ELEMENT);
     gl.enableVertexAttribArray(aColor);
 
+    // Grafika interaktif
+    // Mouse
+    function onMouseClick(event){
+        freeze = !freeze;
+    }
+    document.addEventListener("click", onMouseClick, false);
+
     function render(){
          gl.clearColor(1.0,      0.65,    0.0,    1.0);  // Oranye
          //            Merah     Hijau   Biru    Transparansi
         gl.clear(gl.COLOR_BUFFER_BIT);
-        theta += 0.01;
-        gl.uniform1f(uTheta, theta);
+        if(!freeze){
+            theta += 0.01;
+            gl.uniform1f(uTheta, theta);
+        }
        // var vektor2D = [x, y];
        // gl.uniform2f(uTheta, vektor2D[0], vektor2D[1]);
        // gl.uniform2fv(uTheta, vektor2D);
