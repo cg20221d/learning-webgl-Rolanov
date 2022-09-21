c5dc422a3b3aa869cb87689e97b84a542d77bc3f
Olan <kodokpu@gmail.com>
2022-09-21 13:35:19 +0700

    rainbowww

--- main.js
+++ main.js
@@ -2,34 +2,44 @@ function main() {
     var kanvas = document.getElementById("kanvas");
     var gl = kanvas.getContext("webgl");
 
+    var vertices = [
+        0.5, 0.5, 0.0, 1.0, 1.0,   // A: kanan atas (CYAN)
+        0.0, 0.0, 1.0, 0.0, 1.0,   // B: bawah tengah (MAGENTA)
+        -0.5, 0.5, 1.0, 1.0, 0.0,  // C: kiri atas (KUNING)
+        0.0, 1.0, 1.0, 1.0, 1.0  // D : Tengah atas (PUTIH)
+    ];
+    var buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
     // Vertex shader
-    var vertexShaderCode = `
-    precision mediump float;
-    void main () {
-        float x = 0.0;
-        float y = 0.0;
-        gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
-        gl_PointSize = 30.0;
-      }
-      `;
-    
+    var vertexShaderCode =  `
+    attribute vec2 aPosition;
+    attribute vec3 aColor;
+    varying vec3 vColor;
+    void main() {
+        float x = aPosition.x;
+        float y = aPosition.y;
+        gl_PointSize = 10.0;
+        gl_Position = vec4(x, y, 0.0, 1.0);
+        vColor = aColor;
+    }
+    `;
     var vertexShaderObject = gl.createShader(gl.VERTEX_SHADER);
     gl.shaderSource(vertexShaderObject, vertexShaderCode);
-    gl.compileShader(vertexShaderObject); 
+    gl.compileShader(vertexShaderObject);   // sampai sini sudah jadi .o
 
     // Fragment shader
     var fragmentShaderCode = `
     precision mediump float;
-    void main () {
-      float r = 0.0;
-      float g = 0.0;
-      float b = 1.0;
-      gl_FragColor = vec4(r, g, b, 1.0);
+    varying vec3 vColor;
+    void main() {
+        gl_FragColor = vec4(vColor, 1.0);
     }
-  `;
+    `;
     var fragmentShaderObject = gl.createShader(gl.FRAGMENT_SHADER);
     gl.shaderSource(fragmentShaderObject, fragmentShaderCode);
-    gl.compileShader(fragmentShaderObject); 
+    gl.compileShader(fragmentShaderObject);   // sampai sini sudah jadi .o
 
     var shaderProgram = gl.createProgram(); // wadah dari executable (.exe)
     gl.attachShader(shaderProgram, vertexShaderObject);
@@ -37,9 +47,28 @@ function main() {
     gl.linkProgram(shaderProgram);
     gl.useProgram(shaderProgram);
 
-    gl.clearColor(1.0, 0.5, 0.0, 1.0); // Merah, Hijau, Biru, Transparansi
+    //  Kita mengajari GPU bagaimana caranya mengoleksi
+    //  nilai posisi dari ARRAY_BUFFER
+    //  untuk setiap verteks yang sedang diproses
+    var aPosition = gl.getAttribLocation(shaderProgram, "aPosition");
+    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 
+        5 * Float32Array.BYTES_PER_ELEMENT, 
+        0 * Float32Array.BYTES_PER_ELEMENT);
+    gl.enableVertexAttribArray(aPosition);
+    var aColor = gl.getAttribLocation(shaderProgram, "aColor");
+    gl.vertexAttribPointer(aColor, 3, gl.FLOAT, false, 
+        5 * Float32Array.BYTES_PER_ELEMENT, 
+        2 * Float32Array.BYTES_PER_ELEMENT);
+    gl.enableVertexAttribArray(aColor);
+
+    gl.clearColor(1.0,      0.65,    0.0,    1.0);  // Oranye
+    //            Merah     Hijau   Biru    Transparansi
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    gl.drawArrays(gl.POINTS, 0, 1);
-
+    // gl.drawArrays(gl.POINTS, 0, 11);
+    //gl.drawArrays(gl.TRIANGLE_STRIP, 0 , 2);
+   // gl.drawArrays(gl.LINE_LOOP, 0 , 4);
+   gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
+   // gl.drawArrays(gl.LINE_LOOP, 0, 3); sambungan titik titik
+   // gl.drawArrays(gl.TRIANGLES, 0, 4); // 2 dimensi, segitiga
 }
